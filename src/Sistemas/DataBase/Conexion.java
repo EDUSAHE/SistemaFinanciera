e0101013dbcf2e2a20eb04a080d8f73122339b5d
brandonorjas <brandonorjas.lii@gmail.com>
2022-06-23 19:54:11 -0500

    .

--- src/Sistemas/DataBase/Conexion.java
+++ src/Sistemas/DataBase/Conexion.java
@@ -21,7 +21,7 @@ public class Conexion {
             Connection con=null;
             try{
                 Class.forName("org.hsqldb.jdbcDriver");
-                con = DriverManager.getConnection("jdbc:hsqldb:file:C:\\Users\\EDUARDO\\Documents\\NetBeansProjects\\SistemaFinanciera\\server\\BD","Financiera","");
+                con = DriverManager.getConnection("jdbc:hsqldb:file:C:\\Users\\bran\\Documents\\ProyectoPrestamil\\SistemaFinanciera\\server\\BD","Financiera","");
                 System.out.println("En linea");
             }catch(Exception ex){
                 System.out.println("Fuera de linea"+ex);
